56b04a96d765f8ac499aab49abd1d77738524765
Luis_Alves <33226205+LuisAlves18@users.noreply.github.com>
2021-03-22 22:48:08 +0000

    deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh 
+# abortar no caso de erros 
+set -e 
+# build 
+npm run build 
+# navegar para a pasta com o conteúdo compilado 
+cd dist git init 
+git add -A 
+git commit -m 'deploy' 
+git push -f git@github.com:LuisAlves18/WebPII.git master 
+cd -
