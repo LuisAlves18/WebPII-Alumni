3a2ec66e8ce77e2cf04411ec19972f144c6bbe4c
Luis_Alves <33226205+LuisAlves18@users.noreply.github.com>
2021-05-09 17:36:00 +0100

    optimization

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
       <b-img
         id="landingImg"
         src="https://res.cloudinary.com/dsfbhbeyt/image/upload/v1619953742/carousel1_jktwrb.webp"
+        
         fluid
       ></b-img>
     </div>
